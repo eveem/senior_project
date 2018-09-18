332395b3004a3d17b1a2b8243ae9b0f59516625f
eveem <monpriya.tammavong@gmail.com>
2018-09-18 15:13:13 +0700

    count word each law

--- law_word_count.py
+++ law_word_count.py
@@ -3,4 +3,20 @@ import pickle
 with open('law_with_words.pickle', 'rb') as f:
     data = pickle.load(f)
 
-print(data)
+n = []
+
+for law_dict in data:
+    for law_no in law_dict:
+        new = {}
+        for word in law_dict[law_no]:
+            if word in new:
+                new[word] += 1
+            else:
+                new[word] = 1
+        new_d = {law_no: new}
+        n.append(new_d)
+
+with open('law_words_count.pickle', 'wb') as f:
+    pickle.dump(n, f, pickle.HIGHEST_PROTOCOL)
+
+print("Dump complete !!")
