32c46915c6f876395819cb503afdf72bbbd8a976
eveem <monpriya.tammavong@gmail.com>
2019-01-24 11:32:02 +0700

    clean new law text

--- cut_police_text.py
+++ cut_police_text.py
@@ -1,37 +1,83 @@
 import deepcut
 import pickle
 
-text = "สอบสวน นางสาวบุญลอม  ปัดโรคา    ผู้กล่าวหา ให้การว่า ตามวันเวลาที่เกิดเหตุ ได้เลี้ยงหลานอยู่ที่บ้านพัก ต่อมาได้มีนายสุรชัย จินตนาการ สามีผู้เสียหายมาพูดว่า  ผู้ต้องหาได้พูดที่ตลาดว่า ผู้เสียหาย ไปให้เขาเย็ดทั่วบ้านทั่วเมืองซึ่งผู้เสียหายได้ยินแล้วเกิดความเสียหาย เสียชื่อเสียง จึงได้เดินทางมาแจ้งความร้องทุกข์ให้ดำเนินคดีกับผู้ต้องหาฐานหมิ่นประมาท"
+file_path = '../text_legal_for_project/input/'
 
-words = deepcut.tokenize(text)
+for i in range(2, 41):
+# for i in range(2, 3):
+    file_name = str(i).zfill(4) + '.txt'
+    f = open(file_path + file_name, 'r')
+    text = ''
+    for line in f:
+        text += line
+    words = deepcut.tokenize(text)
+    f.close()
 
-len_doc = len(words)
+    len_doc = len(words)
 
-with open('words_list.pickle', 'rb') as f:
-    words_list = pickle.load(f)
+    with open('words_list.pickle', 'rb') as f:
+        words_list = pickle.load(f)
 
-vector_text = [0 for i in range(0, len(words_list))]
+    vector_text = [0 for i in range(0, len(words_list))]
 
-for word in words:
-    if word in words_list:
-        vector_text[words_list.index(word)] += 1
+    for word in words:
+        if word in words_list:
+            vector_text[words_list.index(word)] += 1
+        
+    for i in range(0, len(words_list)):
+        vector_text[i] /= len_doc
+
+    with open('law_vector.pickle', 'rb') as f:
+        law_vector = pickle.load(f)
+
+    sim = {}
+
+    for no_law in law_vector:
+        p = 0
+        summ = 0
+        for vc in law_vector[no_law]:
+            summ += vc * vector_text[p]
+            p += 1
+        sim[no_law] = summ
+
+    sim_sort = sorted(sim, key=lambda x: sim[x])
+
+    x = open('./result/' + file_name, 'w+')
+    x.write(str(sim_sort))
+    x.close()
+    print(sim_sort[-10:])
+
+# text = "สอบสวน นางสาวบุญลอม  ปัดโรคา    ผู้กล่าวหา ให้การว่า ตามวันเวลาที่เกิดเหตุ ได้เลี้ยงหลานอยู่ที่บ้านพัก ต่อมาได้มีนายสุรชัย จินตนาการ สามีผู้เสียหายมาพูดว่า  ผู้ต้องหาได้พูดที่ตลาดว่า ผู้เสียหาย ไปให้เขาเย็ดทั่วบ้านทั่วเมืองซึ่งผู้เสียหายได้ยินแล้วเกิดความเสียหาย เสียชื่อเสียง จึงได้เดินทางมาแจ้งความร้องทุกข์ให้ดำเนินคดีกับผู้ต้องหาฐานหมิ่นประมาท"
+
+# words = deepcut.tokenize(text)
+
+# len_doc = len(words)
+
+# with open('words_list.pickle', 'rb') as f:
+#     words_list = pickle.load(f)
+
+# vector_text = [0 for i in range(0, len(words_list))]
+
+# for word in words:
+#     if word in words_list:
+#         vector_text[words_list.index(word)] += 1
     
-for i in range(0, len(words_list)):
-    vector_text[i] /= len_doc
+# for i in range(0, len(words_list)):
+#     vector_text[i] /= len_doc
 
-with open('law_vector.pickle', 'rb') as f:
-    law_vector = pickle.load(f)
+# with open('law_vector.pickle', 'rb') as f:
+#     law_vector = pickle.load(f)
 
-sim = {}
+# sim = {}
 
-for no_law in law_vector:
-    p = 0
-    summ = 0
-    for vc in law_vector[no_law]:
-        summ += vc * vector_text[p]
-        p += 1
-    sim[no_law] = summ
+# for no_law in law_vector:
+#     p = 0
+#     summ = 0
+#     for vc in law_vector[no_law]:
+#         summ += vc * vector_text[p]
+#         p += 1
+#     sim[no_law] = summ
 
-sim_sort = sorted(sim, key=lambda x: sim[x])
+# sim_sort = sorted(sim, key=lambda x: sim[x])
 
-print(sim_sort[-10:])
+# print(sim_sort[-10:])
--- formatter.py
+++ formatter.py
@@ -1,44 +1,57 @@
 import deepcut
 import pickle
 
-f = open('criminal_code.html', 'r')
+# f = open('criminal_code.html', 'r')
+f = open('cleaning_law_list.txt', 'r')
 
 law_list = []
 prev_line = []
 
 for line in f:
-    new_line = line.replace('<', '')
-    new_line = new_line.replace('>', '')
-    new_line = new_line.replace('/', '')
-    new_line = new_line.replace('p', '')
-    new_line = new_line.replace('b', '')
-    new_line = new_line.replace('r', '')
-    new_line = new_line.replace('\xa0', '')
+    d = {}
+    new_line = line.replace("'", '')
+    new_line = new_line.replace("[", '')
+    new_line = new_line.replace("]", '')
+    new_line = new_line.replace(",", '')
     new_line = new_line.replace('\n', '')
-    space_split = new_line.split(' ')
-    law_list.append(space_split)
+    new_line = new_line.split(' ')
+    text_inline = ''.join(new_line[2:])
+    d[new_line[1]] = deepcut.tokenize(text_inline)
+    print(d[new_line[1]])
+    law_list.append(d)
+    # print(new_line)
+#     new_line = line.replace('<', '')
+#     new_line = new_line.replace('>', '')
+#     new_line = new_line.replace('/', '')
+#     new_line = new_line.replace('p', '')
+#     new_line = new_line.replace('b', '')
+#     new_line = new_line.replace('r', '')
+#     new_line = new_line.replace('\xa0', '')
+#     new_line = new_line.replace('\n', '')
+#     space_split = new_line.split(' ')
+#     law_list.append(space_split)
 
 # print(len(law_list))
-prev_law = law_list[0]
-fin_law = []
+# prev_law = law_list[0]
+# fin_law = []
 
-for law in law_list[1:]:
-    if law[0] != 'มาตรา':
-        prev_law += law
-    else:
-        fin_law.append(prev_law)
-        prev_law = law
+# for law in law_list[1:]:
+#     if law[0] != 'มาตรา':
+#         prev_law += law
+#     else:
+#         fin_law.append(prev_law)
+#         prev_law = law
 
-fin_law = fin_law[1:]
-dict_format = []
+# fin_law = fin_law[1:]
+# dict_format = []
 
-for law in fin_law:
-    all_words = []
-    for text in law[2:]:
-        all_words += deepcut.tokenize(text)
-    dict_format.append({law[1]: all_words})
+# for law in fin_law:
+#     all_words = []
+#     for text in law[2:]:
+#         all_words += deepcut.tokenize(text)
+#     dict_format.append({law[1]: all_words})
 
 with open('law_with_words.pickle', 'wb') as f:
-    pickle.dump(dict_format, f, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(law_list, f, pickle.HIGHEST_PROTOCOL)
 
 print("Dump complete !!")
